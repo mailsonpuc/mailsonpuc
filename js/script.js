25e5eb03db0bb2373f026d21de875ca0ff54c88e
mailsonpuc <mailson.costa@proton.me>
2025-09-29 13:21:18 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -24,11 +24,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     <img src="${imgSrc}" style="max-width: 100%; height: auto; border-radius: 8px;">
                     <br><br>
                     <p>
-                    API RESTful de barbearia em ASP.NET Core completa, 
+                    API RESTful de distros linux em ASP.NET Core completa, 
                     Backend com arquitetura limpa, integração com Azure SQL e padrões de design aplicados.
+                    FrontEnd em vite React + TypeScript.
                     </p>
                     
-                    <a href="https://github.com/mailsonpuc/pmv-sint-2025-1-e5-proj-movext-t1-barberconnect" target="_blank" style="display: inline-block; margin-top: 15px;">
+                    <a href="https://github.com/mailsonpuc/LinuxApi" target="_blank" style="display: inline-block; margin-top: 15px;">
                     <button class="call-to-action">Ver no GitHub</button>
                     </a>`,
                     showCloseButton: true,
@@ -51,7 +52,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                      Desenvolvimento de uma aplicação full-stack com front-end em React e back-end em ASP.NET Core. 
                      O projeto inclui um sistema de login e autenticação de usuário usando Identity.
                      Além disso, a aplicação se conecta a uma API externa de filmes para buscar e exibir dados,
-                    oferecendo uma experiência completa de visualização
+                    oferecendo uma experiência completa de visualização.
                     </p>
                     
                     <a href="https://github.com/mailsonpuc/primeFlixReact" target="_blank" style="display: inline-block; margin-top: 15px;">
@@ -68,7 +69,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             else if (imgSrc.endsWith('projeto3.png')) {
                 console.log("Projeto 3 clicado");
                 // Código para o Projeto 3
-                  Swal.fire({
+                Swal.fire({
                     title: altText,
                     html: `
                     <img src="${imgSrc}" style="max-width: 100%; height: auto; border-radius: 8px;">
@@ -78,9 +79,35 @@ document.addEventListener('DOMContentLoaded', (event) => {
                      A solução implementa a <b>paginação</b> para dividir o grande conjunto de dados em partes menores e mais gerenciáveis.
                     Essa abordagem melhora significativamente o desempenho e a eficiência da aplicação, 
                     permitindo o retorno sequencial de dados de forma otimizada.
+                    FrontEnd em React com TypeScript.
                      </p>
                     
                     <a href="https://github.com/mailsonpuc/ApiConsumirSqlite" target="_blank" style="display: inline-block; margin-top: 15px;">
+                    <button class="call-to-action">Ver no GitHub</button>
+                    </a>`,
+                    showCloseButton: true,
+                    focusConfirm: false,
+                    confirmButtonText: 'Fechar',
+                    confirmButtonAriaLabel: 'Fechar o pop-up'
+                });
+            }
+
+
+
+            else if (imgSrc.endsWith('projeto4.png')) {
+                console.log("Projeto 4 clicado");
+                // Código para o Projeto 3
+                Swal.fire({
+                    title: altText,
+                    html: `
+                    <img src="${imgSrc}" style="max-width: 100%; height: auto; border-radius: 8px;">
+                    <br><br>
+                     <p>
+                    API RESTful de barbearia em ASP.NET Core completa, 
+                    Backend com arquitetura limpa, integração com Azure SQL e padrões de design aplicados.
+                    </p>
+               
+                    <a href="https://github.com/mailsonpuc/pmv-sint-2025-1-e5-proj-movext-t1-barberconnect" target="_blank" style="display: inline-block; margin-top: 15px;">
                     <button class="call-to-action">Ver no GitHub</button>
                     </a>`,
                     showCloseButton: true,
